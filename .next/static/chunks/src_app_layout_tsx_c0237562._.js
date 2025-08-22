659838cef69495d57149bf1414a63aa89aa50de4
Abubakaralimi <diamondboycares@gmail.com>
2025-08-22 02:13:48 +0200

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755821641241

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_components_ErrorBoundary_tsx_6ad0cb49._.js"
+],
+    source: "dynamic"
+});
